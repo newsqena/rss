2e519f4165ade5e16a79a4c52cc80e07a3476b98
newsqena <newsqena24@gmail.com>
2026-01-28 00:46:32 +0200

    Update987453 main.py

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ def paraphrase_all(title, content):
         print("❌ GEMINI_API_KEY not found")
         return None, None
 
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={api_key}"
+    url = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key={api_key}"
 
     prompt = f"""
 أنت خبير SEO وصحفي عربي محترف. مهمتك هي إعادة صياغة الخبر التالي ليتصدر نتائج البحث ويحقق معايير Google Discover.
@@ -258,7 +258,7 @@ def main():
         if not new_title or not new_content:
             continue
 
-        new_content = trim_to_max_length(new_content, 1600)
+        new_content = trim_to_max_length(new_content, 10000)
 
         final_img = img
         if img:
@@ -293,5 +293,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
